d626c5c35196bae8c490daf2e1a5bd5bc37256e6
Oscar Reula <oreula@nabucodonosor.ccad.unc.edu.ar>
2025-10-07 10:34:37 -0300

    adding a single function with all the methods. Seems to work very well.

--- toy_MHD_auxfunctions.jl
+++ toy_MHD_auxfunctions.jl
@@ -78,6 +78,48 @@ function F_hyp!(u, t, par)
     return du[:]
 end
 
+function F_total!(u, t, par)
+    J, Dx, Δx, Dy, Δy, σx, σy, V, κ, τ, c2, du = par
+    du .=0.0
+    u_a = reshape(u,(D+1,J...))
+    Du_a = reshape(du,(D+1,J...))
+    div = zeros(J...)
+    #first the divergence cleaning term
+    @threads for j in 1:J[2]
+        mul!(view(div,:,j), Dx, view(u_a,1,:,j))
+    end
+    @threads for i in 1:J[1]
+        mul!(view(div,i,:), Dy, view(u_a,2,i,:), one(eltype(u_a)), one(eltype(u_a)))
+    end
+    
+    # the y derivative term 
+    @threads for i in 1:J[1]
+        mul!(view(Du_a,1,i,:), Dy, view(u_a,1,i,:).*V[2,i,:] - view(u_a,2,i,:).*V[1,i,:])
+        mul!(view(Du_a,2,i,:), Dy, view(u_a,D+1,i,:), one(eltype(u_a)), zero(eltype(u_a)))
+        mul!(view(Du_a,2,i,:), Dy, view(div,i,:), κ, one(eltype(u_a)))
+        mul!(view(Du_a,D+1,i,:), Dy, view(u_a,2,i,:), c2, zero(eltype(u_a)))
+        Du_a[D+1,i,:] .+= - τ*u_a[D+1,i,:]
+    end
+    # the x derivative terms
+    @threads for j in 1:J[2]
+        mul!(view(Du_a,2,:,j), Dx , view(u_a,2,:,j).*V[1,:,j] - view(u_a,1,:,j).*V[2,:,j], one(eltype(u_a)), one(eltype(u_a)))
+        mul!(view(Du_a,D+1,:,j), Dx, view(u_a,1,:,j), c2, one(eltype(u_a)))
+        mul!(view(Du_a,1,:,j), Dx , view(u_a,D+1,:,j), one(eltype(u_a)), one(eltype(u_a)))
+        mul!(view(Du_a,1,:,j), Dx , view(div,:,j), κ, one(eltype(u_a)))
+    end
+    #=
+    @threads for i in 1:J[1]
+        mul!(view(Du_a,1,i,:), Δy, view(u_a,1,i,:), σy, one(eltype(u_a)))
+        mul!(view(Du_a,2,i,:), Δy, view(u_a,2,i,:), σy, one(eltype(u_a)))
+    end
+    @threads for j in 1:J[2]
+        mul!(view(Du_a,1,:,j), Δx, view(u_a,1,:,j), σx, one(eltype(u_a)))
+        mul!(view(Du_a,2,:,j), Δx, view(u_a,2,:,j), σx, one(eltype(u_a)))
+    end
+    =#
+    return du[:]
+end
+
 function get_Energy(u,J,Box)
     return norm(u)^2*volume(Box)/prod(J)
 end
